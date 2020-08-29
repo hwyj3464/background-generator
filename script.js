43ed7063865d7d6e004427f7863eda19d911904b
hwyj3464 <hwyj3464@gmail.com>
2020-08-29 14:53:50 -0400

    adding starting project

---
+++ script.js
@@ -0,0 +1,50 @@
+var database = [
+{
+	username: "yoonju",
+	password: "supersecret"
+},
+
+{
+	username: "sally",
+	password: "123"
+},
+
+{
+	username: "ingrid",
+	password: "777"
+}
+];
+
+var newsFeed = [
+{
+	username: "Bobby",
+	timeline: "So tieed from all that learning"
+},
+{
+	username: "Sally",
+	timeline: "Javascript is soooo coool!"
+}];
+
+function isUserValid(username, password){
+	for(var i=0; i<database.length; i++){
+		if(database[i].username === username &&
+			database[i].password ===password){
+			console.log(newsFeed);
+		return true;
+	} 
+}
+	return false;
+}
+
+function signIn(username, password){
+	if (isUserValid(username, password)){
+		console.log(newsFeed);
+	}else{
+		alert("Sorry, wrong username and password!");
+	}
+}
+
+var userNamePrompt = prompt("What's your username?");
+var passwordPrompt = prompt("What's your password?");
+
+signIn(userNamePrompt, passwordPrompt);
